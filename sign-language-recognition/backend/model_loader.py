f9059fa5cba73036f88bbb9c2c552eb230351863
dhruv-upmanyu <dhruv.upmanyu95@gmail.com>
2026-03-14 11:57:34 +0530

    model_loader.py

--- sign-language-recognition/backend/model_loader.py
+++ sign-language-recognition/backend/model_loader.py
@@ -7,12 +7,13 @@ class ModelLoader:
     @staticmethod
     def load_model():
         if ModelLoader.model is None:
+            print("Downloading model from HuggingFace...")
             model_path = hf_hub_download(
                 repo_id="dhruv-upmanyu/sign-language-recognition-model",
                 filename="model.h5"
             )
-
+            print("Model path:", model_path)
             ModelLoader.model = load_model(model_path)
             print("Model loaded successfully!")
-
+            
         return ModelLoader.model
